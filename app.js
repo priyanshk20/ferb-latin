28bd2ede6e5ddb100e258ce6f0578e0ba36bbf12
Priyansh Khandelwal <khandelwalpriyansh85@gmail.com>
2021-06-07 01:34:43 +0530

    app done

---
+++ app.js
@@ -0,0 +1,27 @@
+var btnTranslate = document.querySelector("#select-btn");
+var txtArea = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
+
+var url = "https://api.funtranslations.com/translate/ferb-latin.json";
+
+function getTranslationurl(text){
+    return url + "?" + "text=" + text
+}
+
+function errorHandler(error){
+    console.log("Error Occured" , error);
+}
+
+
+btnTranslate.addEventListener("click" , function clickHandler(){
+    var txtInput = txtArea.value;
+ 
+    fetch(getTranslationurl(txtInput))
+ .then(response => response.json())
+ .then(json => {
+     var translatedText = json.contents.translated;
+     outputDiv.innerText = translatedText;
+ })
+ .catch(errorHandler)
+
+});
